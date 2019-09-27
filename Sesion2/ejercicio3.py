f2224e89f3ae35e15fd92c4b88c030e07f43f780
Vladislav Nikolov <vladis890@gmail.com>
2019-09-27 11:45:20 +0200

    Improved exercise 3.

--- Sesion2/ejercicio3.py
+++ Sesion2/ejercicio3.py
@@ -1,26 +1,33 @@
-
 def normalizar_sexagesimal(tiempo):
     tiempo_normalizado = tiempo % 60
     resto = tiempo // 60
 
     return tiempo_normalizado, resto
 
+
 def normalizar_horas(horas):
     return horas % 24
+
+
+def normalizar_h_min_s(horas, minutos, segundos):
+    # Normalizar los segundos y obtener el tiempo sobrante
+    segundos_final, resto = normalizar_sexagesimal(segundos)
+    
+    # Incrementar los minutos de ser necesario y normalizarlos
+    minutos += resto
+    minutos_final, resto = normalizar_sexagesimal(minutos)
+    
+    # Incrementar las horas de ser necesario y normalizarlas
+    horas += resto
+    horas_final = normalizar_horas(horas)
+
+    return horas_final, minutos_final, segundos_final
+
 
 horas = int(input('Introduzca las horas: '))
 minutos = int(input('Introduzca los minutos: '))
 segundos = int(input('Introduzca los segundos: '))
 
-# Normalizar los segundos y obtener el tiempo sobrante
-segundos_final, resto = normalizar_sexagesimal(segundos)
-
-# Incrementar los minutos de ser necesario y normalizarlos
-minutos += resto
-minutos_final, resto = normalizar_sexagesimal(minutos)
-
-# Incrementar las horas de ser necesario y normalizarlas
-horas += resto
-horas_final = normalizar_horas(horas)
+horas_final, minutos_final, segundos_final = normalizar_h_min_s(horas, minutos, segundos)
 
 print('El tiempo final es: {}h {}min {}s'.format(horas_final, minutos_final, segundos_final))
