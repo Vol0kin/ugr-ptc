1974870c1246683b47fc195cff02b8d9fffbc093
Vladislav Nikolov <vladis890@gmail.com>
2019-09-27 21:18:56 +0200

    Added comments to S2E1,2

--- Sesion2/ejercicio1.py
+++ Sesion2/ejercicio1.py
@@ -1,8 +1,13 @@
+# Entrada de datos
 precio_bruto = float(input('Introduzca el precio bruto: '))
 porc_ganancia = float(input('Introduzca el porcentaje de ganancia del vendedor: '))
 iva = float(input('Introduzca el porcentaje de IVA: '))
 
+# Calculo del precio base
 precio_base = precio_bruto * (1 + porc_ganancia / 100)
+
+# Calculo del precio total
 precio = precio_base * (1 + iva / 100)
 
+# Salida por pantalla
 print('El precio final del vehiculo es {} euros'.format(precio))
--- Sesion2/ejercicio2.py
+++ Sesion2/ejercicio2.py
@@ -1,17 +1,44 @@
 from math import sqrt
 
 def calcular_media(x1, x2, x3):
+    """
+    Funcion que permite calcular la media de tres numeros.
+
+    Args:
+        x1: Primer numero.
+        x2: Segundo numero.
+        x3: Tercer numero.
+    Return:
+        Devuelve la media de los tres numeros de entrada.
+    """
     return (x1 + x2 + x3) / 3
 
 def calcular_desviacion(x1, x2, x3, media):
+    """
+    Funcion que calcula la desviacion de tres numeros, utilizando la media de
+    estos.
+
+    Args:
+        x1: Primer numero.
+        x2: Segundo numero.
+        x3: Tercer numero.
+        media: Media de los tres numeros.
+    Return:
+        Devuelve la desviacion tipica de los tres numeros.
+    """
     return sqrt(((x1 - media) ** 2 + (x2 - media) ** 2 + (x3 - media) ** 2) / 3)
 
+# Entrada de datos
 x1 = float(input('Introduzca el numero x1: '))
 x2 = float(input('Introduzca el numero x2: '))
 x3 = float(input('Introduzca el numero x3: '))
 
+# Calcular media
 media = calcular_media(x1, x2, x3)
+
+# Calcular desviacion
 desviacion = calcular_desviacion(x1, x2, x3, media)
 
+# Salida por pantalla
 print('La desviacion tipica es {}'.format(desviacion))
 
