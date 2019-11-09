270be67e784567139a548dc1e8cffd0bdc5f0eec
Vladislav Nikolov <vladis890@gmail.com>
2019-11-09 17:45:52 +0100

    Acaba primer ejercicio

--- poblacion/utils/html_writer.py
+++ poblacion/utils/html_writer.py
@@ -1,23 +1,108 @@
-def write_population_var_province(pop_prov_info, out_file):
-    # Establecer elementos iniciales del HTML de salida
-    out_html = """<!DOCTYPE html>
-    <html>
-        <head>
-            <title>Variación población provincias 2011-2017</title>
-            <link rel="stylesheet" href="style/estilo.css">
-            <meta charset="utf8">
-        </head>
-        <body>
-            <table>
-                <tr>
+import os
+import locale
+from decimal import Decimal, getcontext
 
-    """
+class HTMLWorker:
+    def __init__(self, title):
+        self.html_begin = f"""<!DOCTYPE html>
+        <html>
+            <head>
+                <title>{title}</title>
+                <link rel="stylesheet" href="../style/estilo.css">
+                <meta charset="utf8">
+            </head>
+            <body>
+                <table>
+        """
 
-    # Establecer elementos finales del HTML de salida
-    out_html += """
-            </table>
-        </body>
-    </html>
-    """
-    with open(out_file, "w", encoding="utf-8") as f:
-        f.write(out_html)
+        self.html_end = """
+                </table>
+            </body>
+        </html>
+        """
+
+        locale.setlocale(locale.LC_ALL, 'es_ES.utf8')
+        getcontext().prec = 2
+    
+    def write_HTML_output(self, output, out_file):
+
+        # Establecer directorio de salida
+        result_dir = "resultados/"
+
+        # Crear directorio de salida en caso de que no exista
+        if not os.path.exists(result_dir):
+            os.mkdir(result_dir)
+        
+        # Escribir salida al fichero
+        with open(result_dir + out_file, "w", encoding="utf-8") as f:
+            f.write(output)
+    
+    def write_population_var_province_years(self, pop_prov_info, types, years, out_file):
+        # Añadir informacion inicial a la salida
+        output = self.html_begin
+
+        """
+        Escribir primera linea de la salida, donde estaran
+        algunas de las cabeceras
+        """
+        output += """<tr>
+            <th rowspan="2"></th>
+        """
+
+        # Escribir los tipos de variaciones
+        for type in types:
+            output += f'<th colspan="7">{type}</th>\n'
+        
+        output += "</tr>\n"
+
+        """
+        Escribir segunda linea de la salida, donde estaran los años
+        """
+        output += "<tr>\n"
+
+        """
+        Escribir los años (se repite tantas veces el numero de años como
+        tipos de variacion haya
+        """
+        for year in years * len(types):
+            output += f"<th>{year}</th>\n"
+        
+        output += "</tr>\n"
+
+        """
+        Escribir contenido de la tabla
+        prov -> Provincias
+        pop_info -> {tipo_variacion: {año: poblacion}}
+        """
+        for prov, pop_info in pop_prov_info.items():
+            # Escribir provincia
+            output += f"""<tr>
+            <th>{prov}</th>
+            """
+
+            """
+            Obtener años y poblacion asociada a cada año para cada tipo
+            de variacion
+            year_pop -> {año: poblacion} (un diccionario para cada tipo
+            de variacion; dos diccionarios en total)
+            """
+            for year_pop in pop_info.values():
+                # Obtener poblacion para cada año
+                for pop in year_pop.values():
+                    # Formatear salida segun el tipo y añadirla
+                    if isinstance(pop, float):
+                        value = locale.format_string("%.2f", Decimal(str(pop)), grouping=True)
+                    else:
+                        value = locale.format_string("%d", pop, grouping=True)
+                    
+                    output += f"<td>{value}</td>\n"
+
+            output += "</tr>\n"
+        
+        output += "</tr>\n"
+
+        # Añadir informacion final a la salida
+        output += self.html_end
+
+        # Escribir salida
+        self.write_HTML_output(output, out_file)
--- poblacion/var_poblacion_prov.py
+++ poblacion/var_poblacion_prov.py
@@ -1,9 +1,12 @@
 from utils import read_csv
+from utils.html_writer import HTMLWorker
 import numpy as np
 
-def get_population_variation_province(filename):
-    # Obtener un diccionario con la informacion sobre la poblacion
-    # a partir del fichero CSV
+def population_variation_province(filename):
+    """
+    Obtener un diccionario con la informacion sobre la poblacion
+    a partir del fichero CSV
+    """
     population_dict = read_csv.read_csv_file(filename)
 
     # Crear diccionario que contendra la informacion de salida
@@ -12,25 +15,37 @@ def get_population_variation_province(filename):
     # Crear lista con las claves para los tipos de variacion
     variation_keys = ["Variación absoulta", "Variación relativa"]
 
+    # Procesar informacion
     for prov, pop_info in population_dict.items():
-        pop_years_num = pop_info["Total"]
+        # Obtener informacion sobre la poblacion total
+        years_pop = pop_info["Total"]
 
-        years_list = [year for year in pop_years_num.keys()]
-        population = np.array([pop for pop in pop_years_num.values()], dtype=np.int)
+        """
+        Obtener los años y los años de salida (todos menos el primer año,
+        el de valor mas bajo)
+        """
+        years_list = [year for year in years_pop.keys()]
+        output_years = years_list[:-1]
 
+        # Obtener poblacion de cada año
+        population = np.array([pop for pop in years_pop.values()], dtype=np.int)
+
+        # Calcular variaciones
         abs_variation = population[:-1] - population[1:]
         rel_variation = (abs_variation / population[1:]) * 100
 
-        # Crear diccionario que contiene los tipos de variacion y los
-        # valores calculados
+        """
+        Crear diccionario que contiene los valores calculados para cada
+        tipo de variacion
+        """
         variation_dict = {type: variation for type, variation in zip (variation_keys, [abs_variation, rel_variation])}
 
         # Guardar la informacion calculada para cada provincia
-        pop_variation_dict[prov] = {type: {year: variation for year, variation in zip(years_list, variation_dict[type])}
+        pop_variation_dict[prov] = {type: {year: variation for year, variation in zip(output_years, variation_dict[type])}
                                     for type in variation_keys}
-
-        
-    print(pop_variation_dict)
+    
+    # Escribir salida en fichero HTML
+    writer = HTMLWorker("Variación población provincias 2011-2017")
+    writer.write_population_var_province_years(pop_variation_dict, variation_keys, output_years,
+                                               "variacionProvincias2011-17.htm")
     return pop_variation_dict
-
-get_population_variation_province("datos/poblacionProvinciasHM2010-17.csv")
